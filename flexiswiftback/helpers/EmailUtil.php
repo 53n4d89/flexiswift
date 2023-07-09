dfaac8086f10084ae55c796da72d123d31743034
53n4d89 <120484854+53n4d89@users.noreply.github.com>
2023-07-09 16:09:51 +0200

    Update EmailUtil.php

--- flexiswiftback/helpers/EmailUtil.php
+++ flexiswiftback/helpers/EmailUtil.php
@@ -13,15 +13,15 @@ class EmailUtil {
             // Server settings
             $mail->SMTPDebug = 0; // Enable verbose debug output
             $mail->isSMTP(); // Set mailer to use SMTP
-            $mail->Host = 'mail.sarajevoweb.com'; // Specify main and backup SMTP servers
+            $mail->Host = ''; // Specify main and backup SMTP servers
             $mail->SMTPAuth = true; // Enable SMTP authentication
-            $mail->Username = 'sarajevoweb@sarajevoweb.com'; // SMTP username
-            $mail->Password = ':~i9#^BVK4]=gQp'; // SMTP password
+            $mail->Username = ''; // SMTP username
+            $mail->Password = ''; // SMTP password
             $mail->SMTPSecure = 'tls'; // Enable TLS encryption, 'ssl' also accepted
             $mail->Port = 587; // TCP port to connect to
 
             // Sender and recipient settings
-            $mail->setFrom('sarajevoweb@sarajevoweb.com', 'Mailer');
+            $mail->setFrom('', 'Mailer');
             $mail->addAddress($email); // Add a recipient
 
             // Content
